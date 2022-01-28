e13fc06f69d66a64af4ece6cd71f652f17d5daaa
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2022-01-28 16:03:04 +0900

    Figmaのコンポーネントを表示

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import './App.css';
+import "./App.css";
+import {
+  NewHomes,
+  NavBar,
+  MarketingFooter,
+} from "./ui-components";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
+      <NewHomes />
+      <MarketingFooter />
     </div>
   );
 }
